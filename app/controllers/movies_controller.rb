98c04a46ef23d70348a819901140c0fb3af6968b
joshuaokeeffe <joshokeeffecollege@gmail.com>
2025-11-11 13:34:20 +0000

    Add styling and navbar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -25,7 +25,7 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie, notice: "Movie was successfully created." }
+        format.html { redirect_to movies_path, notice: "Movie was successfully added to your library." }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class MoviesController < ApplicationController
     @movie.destroy!
 
     respond_to do |format|
-      format.html { redirect_to movies_path, notice: "Movie was successfully destroyed.", status: :see_other }
+      format.html { redirect_to movies_path, notice: "Movie was successfully deleted.", status: :see_other }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
-  root "searches#index"
+  root "movies#index"
   resources :movies
   get "/search", to: "searches#index"
 end
