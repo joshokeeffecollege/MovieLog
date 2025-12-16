ea07f584be2fd6a4710b20fcb1cb97582ed13578
Josh <joshokeeffecollege@gmail.com>
2025-12-16 20:03:23 +0000

    Get ready for deployment

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,8 @@
 # Enable CORS for specified origins to allow cross-origin requests
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Specify allowed origins for CORS
-    origins "http://localhost:5173", "http://127.0.0.1:5173"
+    # Specify allowed origins for CORS (add your production frontend URL here)
+    origins "http://localhost:5173", "http://127.0.0.1:5173", ENV["FRONTEND_URL"]
 
     # Define allowed resource paths, headers, and HTTP methods
     resource "*",
